b9f217b2f250344579f54dbd76ed01ddd979c89a
erichiroshi <erichiroshi@gmail.com>
2024-02-21 06:50:17 -0400

    4.25. Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.erichiroshi.algafood.api.controller;
 
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,13 +25,27 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscarId(@PathVariable Long cozinhaId) {
         Optional<Cozinha> optionalCozinha = repository.findById(cozinhaId);
-        if(optionalCozinha.isPresent())
+        if (optionalCozinha.isPresent())
             return ResponseEntity.ok(optionalCozinha.get());
         return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha){
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
         return ResponseEntity.created(null).body(repository.save(cozinha));
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Optional<Cozinha> optionalCozinha = repository.findById(cozinhaId);
+
+        if (optionalCozinha.isPresent()) {
+            Cozinha cozinhaAtual = optionalCozinha.get();
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaAtual = repository.save(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
