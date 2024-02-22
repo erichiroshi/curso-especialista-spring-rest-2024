32d6cec9c082bb5b4dce8f5feae9bee54ec40b20
erichiroshi <erichiroshi@gmail.com>
2024-02-22 06:55:44 -0400

    6.1. Mapeando relacionamento bidirecional com @OneToMany e @JsonIgnore

--- src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
+++ src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
@@ -1,7 +1,11 @@
 package com.erichiroshi.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -17,5 +21,9 @@ public class Cozinha {
 
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
