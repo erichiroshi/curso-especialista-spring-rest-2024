c2a4e120e534332a3323ad3e29a6c3aea8256df0
erichiroshi <erichiroshi@gmail.com>
2024-02-22 08:08:25 -0400

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/erichiroshi/algafood/domain/model/Grupo.java
@@ -0,0 +1,29 @@
+package com.erichiroshi.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Entity
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private final List<Permissao> permissoes = new ArrayList<>();
+
+}
