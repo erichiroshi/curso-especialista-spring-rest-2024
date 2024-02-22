75a46de2bb4acca3907acaa56459ab1c21cd7b8e
erichiroshi <erichiroshi@gmail.com>
2024-02-22 07:11:24 -0400

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/erichiroshi/algafood/domain/model/Restaurante.java
+++ src/main/java/com/erichiroshi/algafood/domain/model/Restaurante.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -26,4 +28,10 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+        joinColumns = @JoinColumn(name = "restaurante_id"),
+        inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
