f208d66b55ca0d555282ed5036bd5bc1c5ae386b
erichiroshi <erichiroshi@gmail.com>
2024-02-26 15:52:51 -0400

    10.10. Criando um método para fazer setup dos testes

--- src/test/java/com/erichiroshi/algafood/CozinhaControllerTest.java
+++ src/test/java/com/erichiroshi/algafood/CozinhaControllerTest.java
@@ -2,12 +2,14 @@ package com.erichiroshi.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CozinhaControllerTest {
@@ -15,17 +17,34 @@ public class CozinhaControllerTest {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
         given()
-            .basePath("/cozinhas")
-            .port(port)
             .accept(ContentType.JSON)
         .when()
             .get()
         .then()
             .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .body("", hasSize(4));
     }
 }
