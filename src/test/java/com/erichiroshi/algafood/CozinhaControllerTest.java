575fcc714f508e88c65189875981ca9e0be09141
erichiroshi <erichiroshi@gmail.com>
2024-02-26 15:58:09 -0400

    10.12. Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/erichiroshi/algafood/CozinhaControllerTest.java
+++ src/test/java/com/erichiroshi/algafood/CozinhaControllerTest.java
@@ -2,8 +2,10 @@ package com.erichiroshi.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,16 @@ public class CozinhaControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
     }
 
     @Test
@@ -46,5 +53,17 @@ public class CozinhaControllerTest {
             .get()
         .then()
             .body("", hasSize(4));
+    }
+
+    @Test
+    public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+        given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
     }
 }
