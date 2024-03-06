e5ac960bbb0d681b9febca3561a8c5a2d8b4bacc
erichiroshi <erichiroshi@gmail.com>
2024-03-06 08:44:58 -0400

    14.3. Validando o tamanho máximo do arquivo

--- src/main/java/com/erichiroshi/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,8 +1,12 @@
 package com.erichiroshi.algafood.api.controller;
 
 import com.erichiroshi.algafood.api.model.dtos.inputs.FotoProdutoInput;
+import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Path;
 import java.util.UUID;
@@ -12,7 +16,7 @@ import java.util.UUID;
 public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput foto) {
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput foto) {
 
         var nomeArquivo = UUID.randomUUID().toString() + "_" + foto.getArquivo().getOriginalFilename();
 
--- src/main/java/com/erichiroshi/algafood/api/model/dtos/inputs/FotoProdutoInput.java
+++ src/main/java/com/erichiroshi/algafood/api/model/dtos/inputs/FotoProdutoInput.java
@@ -1,5 +1,8 @@
 package com.erichiroshi.algafood.api.model.dtos.inputs;
 
+import com.erichiroshi.algafood.core.validation.FileSize;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,7 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Setter
 public class FotoProdutoInput {
 
+    @NotNull
+    @FileSize(max = "500KB")
     private MultipartFile arquivo;
+
+    @NotBlank
     private String descricao;
 
 }
